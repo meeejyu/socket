7a7290e62bf0a0df955eb5ed1b7692a427bb4eb9
meeejyu <alwn04016@naver.com>
2023-03-08 23:58:27 +0900

    feat "jpa, h2 추가 및 memberDto 추가"

--- src/main/java/com/socket/socket/ProtocolServer.java
+++ src/main/java/com/socket/socket/ProtocolServer.java
@@ -23,7 +23,7 @@ public class ProtocolServer {
 				System.out.println("연결 수락됨" + isa.getHostName());
 				// 소켓 -> 서버
 				BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));																	// 데이터를 읽어옴
-				String line = reader.readLine(); // 서버 클래스 안의 line변수 안에 "안녕하세요!"가 저장됨
+				String line = reader.readLine(); // 서버 클래스 안의 line변수 안에 메세지가 저장됨
 				// 서버 -> 소켓
 				PrintWriter writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream())); // 소켓으로 데이터를
 																										// 바깥으로 보냄
